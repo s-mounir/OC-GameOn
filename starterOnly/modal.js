ce30d3ed00a942de859f3de3702e375b4cb79e1f
Sara Mounir <sara_mounir@hotmail.fr>
2023-03-30 18:54:00 +0200

    issue 2 Implémenter entrées du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,48 @@ closeBtn.addEventListener("click", closeModal);
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// validate form
+function validate(e){
+  let validForm = true;
+
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/;
+  
+  const first = document.getElementById("first").value;
+  const last = document.getElementById("last").value;
+  const email = document.getElementById("email").value;
+  const quantity = document.getElementById("quantity").valueAsNumber;
+  const locationCheckbox = document.querySelector('input[name="location"]:checked');
+  const conditionsGenerales = document.getElementById("checkbox1");
+  const formData = document.getElementsByClassName("formData");
+
+  if((first.length < 2) || (first === "")){
+    validForm = false;
+  }
+  if((last.length < 2) || (last === "")){
+    validForm = false;
+  }
+  if(!email.match(validRegex)){
+    validForm = false;
+  }
+  if((typeof quantity !== "number") || (isNaN(quantity))){
+    validForm = false;
+  }
+  if((locationCheckbox === null)){
+    validForm = false;
+  }
+  if(!conditionsGenerales.checked){
+    validForm = false;
+  }
+
+  console.log(validForm);
+  if(!validForm){
+    e.preventDefault();
+    console.log("le formulaire est faux");
+  }else{
+    console.log("tout est bon!");
+  }
+
+  console.log(validForm);
+  return validForm;
+}
