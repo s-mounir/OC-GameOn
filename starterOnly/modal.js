e844044625cac0c66d48c8b73e0472be02e6b12e
Sara Mounir <sara_mounir@hotmail.fr>
2023-04-09 21:02:08 +0200

    function isCorrect to simplify code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,7 +28,20 @@ closeBtn.addEventListener("click", closeModal);
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+  // reset des champs
 }
+
+//check if value is correct, if not add error message
+function isCorrect(condition,num,errorMessage){
+  if(condition){
+    validForm = false;
+    const firstError = document.createElement("p");
+    firstError.classList.add("errorMessage");
+    firstError.innerText = errorMessage;
+    formData[num].appendChild(firstError);
+  }
+}
+
   
   // validate form
   function validate(e){
@@ -45,59 +58,25 @@ function closeModal() {
     const locationCheckbox = document.querySelector('input[name="location"]:checked');
     const conditionsGenerales = document.getElementById("checkbox1");
     const formData = document.getElementsByClassName("formData");
+    const errorMessage = document.querySelectorAll(".errorMessage");
   
-    if((first.length < 2) || (first === "")){
-      validForm = false;
-      const firstError = document.createElement("p");
-      firstError.classList.add("errorMessage");
-      firstError.innerText = "Veillez entrer 2 caractères ou plus pour le champ du prénom";
-      formData[0].appendChild(firstError);
+    if(errorMessage.length > 0){
+      for(let error of errorMessage){
+        error.remove();
+      }
     }
-    if((last.length < 2) || (last === "")){
-      validForm = false;
-      const lastError = document.createElement("p");
-      lastError.classList.add("errorMessage");
-      lastError.innerText = "Veillez entrer 2 caractères ou plus pour le champ du nom";
-      formData[1].appendChild(lastError);
-    }
-    if(!email.match(validRegex)){
-      validForm = false;
-      const emailError = document.createElement("p");
-      emailError.classList.add("errorMessage");
-      emailError.innerText = "Veillez entrer une adresse email valide";
-      formData[2].appendChild(emailError);
-    }
-    if(birthdate === ""){
-      validForm = false;
-      const birthdateError = document.createElement("p");
-      birthdateError.classList.add("errorMessage");
-      birthdateError.innerText = "Veillez entrer votre date de naissance";
-      formData[3].appendChild(birthdateError);
-    }
-    if((typeof quantity !== "number") || (isNaN(quantity))){
-      validForm = false;
-      const quantityError = document.createElement("p");
-      quantityError.classList.add("errorMessage");
-      quantityError.innerText = "Veillez entrer un nombre";
-      formData[4].appendChild(quantityError);
-    }
-    if((locationCheckbox === null)){
-      validForm = false;
-      const locationError = document.createElement("p");
-      locationError.classList.add("errorMessage");
-      locationError.innerText = "Vous devez choisir une option";
-      formData[5].appendChild(locationError);
-    }
-    if(!conditionsGenerales.checked){
-      validForm = false;
-      const conditionsGeneralesError = document.createElement("p");
-      conditionsGeneralesError.classList.add("errorMessage");
-      conditionsGeneralesError.innerText = "Vous devez vérifier que vous acceptez les termes et conditions";
-      formData[6].appendChild(conditionsGeneralesError);
-    }
+
+    isCorrect((first.length < 2) || (first === ""),0,"Veillez entrer 2 caractères ou plus pour le champ du prénom");
+    isCorrect((last.length < 2) || (last === ""),1,"Veillez entrer 2 caractères ou plus pour le champ du nom");
+    isCorrect(!email.match(validRegex),2,"Veillez entrer une adresse email valide");
+    isCorrect(birthdate === "",3,"Veillez entrer votre date de naissance");
+    isCorrect((typeof quantity !== "number") || (isNaN(quantity)),4,"Veillez entrer un nombre");
+    isCorrect((locationCheckbox === null),5,"Vous devez choisir une option");
+    isCorrect(!conditionsGenerales.checked,6,"Vous devez vérifier que vous acceptez les termes et conditions");
 
     if(validForm){
       console.log("ce formulaire est valid");
+      // display none sur le formulaire + ajout texte
       modalBody.innerHTML = '<p class="validMessage" id="closeValid"> Merci pour votre inscription </p> <button class="btn-submit" onclick="closeModal()">Fermer</button>';
     }
 
