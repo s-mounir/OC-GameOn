b27cb050b276e54da481c3a90aa8e173b71f5ee3
Sara Mounir <sara_mounir@hotmail.fr>
2023-04-14 12:28:35 +0200

    correction soutenance

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,11 +9,13 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalContent = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBody = document.querySelector(".modal-body");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 const form = document.querySelector("form");
+const submitBtn = document.querySelector(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,6 +23,21 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+
+  if(window.innerWidth <= 768){
+    modalContent.style.marginTop = "100px";
+    modalContent.style.marginBottom = "0px";
+    modalbg.style.backgroundColor = "rgba(26, 39, 156, 0)";
+
+    var x = document.getElementById("myTopnav");
+    x.style.position = "fixed";
+    x.style.zIndex = 5;
+    x.style.backgroundColor = "white";
+    x.style.height= "100px";
+    x.style.width= "100%";
+    x.style.margin= "0";
+    x.style.padding = "20px";
+  }
 }
 
 // close modal event
@@ -42,13 +59,15 @@ function isCorrect(condition,num,errorMessage){
     firstError.classList.add("errorMessage");
     firstError.innerText = errorMessage;
     formData[num].appendChild(firstError);
+    formData[num].querySelector("input").style.borderColor= "red";
   }
 }
 
   // validate form
   function validate(e){
-    e.preventDefault();
   
+    e.preventDefault();
+
     const validRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
     
     const first = document.getElementById("first").value;
@@ -75,11 +94,19 @@ function isCorrect(condition,num,errorMessage){
     isCorrect((locationCheckbox === null),5,"Vous devez choisir une option");
     isCorrect(!conditionsGenerales.checked,5,"Vous devez vérifier que vous acceptez les termes et conditions");
 
+    console.log(validForm);
+
     if(validForm){
       console.log("ce formulaire est valid");
-      // display none sur le formulaire + ajout texte
-      modalBody.innerHTML = '<p class="validMessage" id="closeValid"> Merci pour votre inscription </p> <button class="btn-submit" onclick="closeModal()">Fermer</button>';
+      modalBody.innerHTML = '<p class="validMessage" id="closeValid"> Merci pour votre inscription </p> <button class="btn-submit" id="closeBtn">Fermer</button>';
+
+      // Submit form event
+      const closeBtnModal = document.getElementById("closeBtn");
+      closeBtnModal.addEventListener("click", closeModal);
     }
 
     return validForm;
   }
+
+// Submit form event
+submitBtn.addEventListener("click", validate);
